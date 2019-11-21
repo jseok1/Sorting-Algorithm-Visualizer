c24064f6f037570d422b2509f7a1c353ac8b6a12
Junyoung Seok <junyoung.seok@mail.utoronto.ca>
2019-11-21 16:12:36 -0500

    Replace quicksort implementation.

--- SortingAlgorithms.py
+++ SortingAlgorithms.py
@@ -76,67 +76,40 @@ def _merge(lst, start, mid, end):
         yield (lst.copy(), {start + i, start, end})
 
 
-# def quick_sort(lst):
-#     """Quick sort implementation in Python."""
-#     yield from _quick_sort(lst, 0, len(lst))
-#
-#
-# def _quick_sort(lst, start, end):
-#     """Mutating quick sort implementation in Python using the Hoare partition
-#     scheme."""
-#     if end - start < 2:
-#         pass
-#     else:
-#         wrapper = set()
-#         yield from _partition(lst, start, end, wrapper)
-#         pivot = wrapper.pop()
-#         yield (lst.copy(), {pivot})
-#         yield from _quick_sort(lst, start, pivot)
-#         yield from _quick_sort(lst, pivot + 1, end)
-#
-#
-# def _partition(lst, start, end, wrapper):
-#     """Helper function for quick sort."""
-#     pivot = lst[start]
-#     i = start + 1
-#     j = end
-#     while i < j:
-#         if lst[i] < pivot:
-#             i += 1
-#         else:
-#             lst[i], lst[j - 1] = lst[j - 1], lst[i]
-#             j -= 1
-#         yield (lst.copy(), {i, j, pivot})
-#     lst[start], lst[i - 1] = lst[i - 1], lst[start]
-#     yield (lst.copy(), {i, j, pivot})
-#     wrapper.add(i - 1)
-
-def quicksort(lst):
-    if len(lst) < 2:   # sorted
-        return lst[:]
-    else:   # unsorted
-        pivot = lst[0]
-        smaller, bigger = _partition(lst[1:], pivot)
-        partitioned = smaller + [pivot] + bigger
-        for i in range(len(lst)):
-            lst[i] = partitioned[i]
-            yield (lst.copy(), {})
-        smaller_sorted = quicksort(smaller)
-        bigger_sorted = quicksort(bigger)
-        return smaller_sorted + [pivot] + bigger_sorted
+def quick_sort(lst):
+    """Quick sort implementation in Python."""
+    yield from _quick_sort(lst, 0, len(lst))
 
 
-def _partition(lst, pivot):
-    smaller = []
-    bigger = []
-    for item in lst:
-        if item <= pivot:
-            smaller.append(item)
+def _quick_sort(lst, start, end):
+    """Mutating quick sort implementation in Python using the Hoare partition
+    scheme."""
+    if end - start < 2:
+        pass
+    else:
+        wrapper = set()
+        yield from _partition(lst, start, end, wrapper)
+        pivot = wrapper.pop()
+        yield (lst.copy(), {pivot})
+        yield from _quick_sort(lst, start, pivot)
+        yield from _quick_sort(lst, pivot + 1, end)
+
+
+def _partition(lst, start, end, wrapper):
+    """Helper function for quick sort."""
+    pivot = lst[start]
+    i = start + 1
+    j = end
+    while i < j:
+        if lst[i] < pivot:
+            i += 1
         else:
-            bigger.append(item)
-    return smaller, bigger
-
-
+            lst[i], lst[j - 1] = lst[j - 1], lst[i]
+            j -= 1
+        yield (lst.copy(), {i, j, pivot})
+    lst[start], lst[i - 1] = lst[i - 1], lst[start]
+    yield (lst.copy(), {i, j, pivot})
+    wrapper.add(i - 1)
 
 
 def heap_sort(lst):
@@ -171,4 +144,3 @@ def _heapify(lst, n, i):
         lst[i], lst[largest] = lst[largest], lst[i]
         yield (lst.copy(), {i, largest, n})
         yield from _heapify(lst, n, largest)
-
